ad4a010666380bf3d3aec23a386dbdf7ca38d0d2
DhiaShams <dhiadhilu123@gmail.com>
2025-06-18 16:00:25 +0530

    Added modals in employee list page

--- src/app/components/employees/employees.ts
+++ src/app/components/employees/employees.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-employees',
   standalone: false,
@@ -12,7 +11,8 @@ export class Employees {
   employees= [
     {name:'Maria',designation:'trainee',salary:'10,000'},
     {name:'Peter',designation:'manager',salary:'30,000'}
-  ]
+  ];
+  public visible = false;
 
   editEmployee(emp: any) {
     console.log('Editing', emp);
@@ -22,3 +22,5 @@ export class Employees {
     console.log('Deleting',emp)
   }
 }
+
+
